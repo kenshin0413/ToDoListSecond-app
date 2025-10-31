650faa412a1fcf03c937cd735b4934c2d5aaf0f7
kenshin0413 <kenshin1234569@gmail.com>
2025-10-31 23:31:45 +0900

    初期

---
+++ ToDoListSecond/ToDoListSecond/ToDoListSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSecondApp.swift
+//  ToDoListSecond
+//
+//  Created by miyamotokenshin on R 7/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
